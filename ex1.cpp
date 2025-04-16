a8105fd1b089b8f040b96ea832fc429926dcadc3
PedroHenrique <143274074+BatatAzul@users.noreply.github.com>
2025-04-16 20:25:22 -0300

    Add files via upload

---
+++ ex1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int altura;
+	int base;
+	int area;
+	
+	cout <<"informe a base do retangulo ";
+	cin >> base;
+	
+	cout << "informe a altura do retangulo ";
+	cin >> altura;
+	
+	area = base * altura;
+	cout << "A area do retangulo:  " << area;
+}
---
+++ ex2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int aresta;
+	int area;
+	
+	cout << "digite o valor da aresta do quadrado: ";
+	cin >> aresta;
+	
+	area = aresta * aresta;
+	cout << "a área do quadrado é: " << area;
+}
